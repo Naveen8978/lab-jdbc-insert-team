aece22470747422d0bb582cb633f569131a0e59a
@naveen8978 <naveengajja27@gmail.com>
2020-07-15 09:00:09 +0530

    prograd

--- src/dao/TeamDAO.java
+++ src/dao/TeamDAO.java
@@ -1,1 +1,27 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import model.City;
+import model.Team;
+import utility.ConnectionManager;
+
+public class TeamDAO{
+	public void createTeam(Team team) {
+		Connection con = ConnectionManager.getConnection();
+		String sql = "Insert into kabbadi(name,coach,cityid,cityname)Values(?,?,?,?);"
+				PreparedStatement st = con.prepareStatement(sql);
+    	        City city = (City) team.getCity();
+	        	st.setString(1, team.getName());
+	    		st.setString(2,team.getCoach());
+	    		st.setLong(4,city.getCityId());
+	        	st.setString(5, city.getCityName());
+	        	st.executeUpdate();
+	    		con.close();
+	    		System.out.println("Team has been created");
+
+
+
+	}
+}
--- src/utility/ConnectionManager.java
+++ src/utility/ConnectionManager.java
@@ -1,1 +1,14 @@
+package utility;
 
+import java.sql.Connection;
+import java.util.Properties;
+
+public class ConnectionManager{
+	public static Connection getConnection() {
+		
+		
+		return con;
+		public static Properties loadPropertiesFile() {
+		
+	}
+}
